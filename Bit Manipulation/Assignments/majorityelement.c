d3118eb672dc59dc2576a0160c021045d77a509a
prasanna12022003 <prasanna.t2020ece@sece.ac.in>
2022-09-09 09:57:01 +0530

    Assignments

---
+++ Bit Manipulation/Assignments/majorityelement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+int majorityElement(int* nums, int numsSize){
+    
+    int major=0,c=1,i;
+    int n=numsSize;
+    for(i=1;i<n;i++)
+    {
+       if (nums[major]==nums[i]){
+            c++;
+          }
+        else
+            c--;
+        if (c==0)
+        {
+            major=i;
+            c=1;
+        }
+    }
+    return nums[major];
+}
