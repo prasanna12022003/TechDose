e0e5dc52b5d2d1afcf50120e735dc3bb2f4621af
prasanna12022003 <prasanna.t2020ece@sece.ac.in>
2022-09-09 09:36:45 +0530

    setIthbit

---
+++ Bit Manipulation/setIthBit.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSetBit(int n,int p)
+{
+    return n & (!(1<<(pos-1));
+}
+
+int main()
+{
+    int n,p;
+    cin>>n>>p;
+    cout<<isSetBit(n,p);
+    return 0;
+}
+Footer
+© 2022 GitHub, Inc.
