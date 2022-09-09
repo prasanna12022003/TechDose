360de64721079ed93379d24ab3f2bea992cec1e5
prasanna12022003 <prasanna.t2020ece@sece.ac.in>
2022-09-09 09:14:11 +0530

    isPrime

---
+++ Math Algorithm/isPrime.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+                                  //time complexity : O(N)
+bool isPrime1(int val){
+    for(int i=2; i<val; i++){
+        if(val%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+                                  //time complexity : O(root(N))
+bool isPrime2(int val){
+    for(int i=2; i*i<=val; i++){
+        if(val%i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int N; 
+    cin >> N;
+    cout << (isPrime1(N) ? "Prime" : "Not Prime") << endl;
+    cout << (isPrime2(N) ? "Prime" : "Not Prime") << endl;
+}
